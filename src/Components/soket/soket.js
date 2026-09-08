7ca5e7655cb93b41a8f3eac6b1ed98b47ae8509d
Mahmud Rafi <rafimahmud548@gmail.com>
2026-09-08 18:47:35 +0600

    Changes activated

--- src/Components/soket/soket.js
+++ src/Components/soket/soket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://job-portal-website-back-end.vercel.app/", {
+export const socket = io("job-portal-website-back-end.vercel.app", {
     withCredentials: true
 });
